da828c339999c2e89a83c28f3285b2f06888e493
Pio Sierra <10895805+piosierra@users.noreply.github.com>
2022-02-09 16:42:00 +0000

    executable done

--- README.md
+++ README.md
@@ -1,69 +1,12 @@
 # Workflow to find very large inversions with kmers.
 
+## 1- Identify large inversions based on mapping of unique k-mers 
 ```
-# Obtain the kmers
-FastK -k31 -t1 CM012070.1.fa 
-Tabex CM012070.1.ktab LIST > kmers_all
-
-FastK -k31 -t1 DA-386_06-X.fa 
-Tabex DA-386_06-X.fa LIST > kmers_all_2
-
-# Get only lines with freq = 1
-awk -F" " '{if ($4 == 1) print $2}' kmers_all > unique_kmers
-awk -F" " '{if ($4 == 1) print $2}' kmers_all_2 > unique_kmers2
-
-# Get just lines common to both samples
-comm -12 <( sort unique_kmers ) <( sort unique_kmers2 ) > unique_kmers_both
-
-
-# Get just a sample
-shuf -n 1000000 unique_kmers_both > unique_kmers_both_sample 
-
-# Convert to fasta
-awk '{print "> seq " ++count ORS $0}' unique_kmers_both_sample > unique_kmers_both_sample.fa
-
-# Align
-bwa mem -a CM012070.1.fa unique_kmers_both_sample.fa > kmers_aligned.sam
-
-# Remove header
-sed '/^@/d' < kmers_aligned.sam > ukmers.sam
-
-# See unique values of first colum
-awk '{ a[$1]++ } END { for (b in a) { print b } }' ukmers.sam
-
-# Remove lines with more than one hit (next line is 256 or 272, then remove rest of 256/272)
-awk 'NR > 1 && !(/\t272\t/ || /\t256\t/) { print p } { p = $0 } END { print}' ukmers.sam > ukmers_clean1.sam
-sed '/^\t\(272\|256\|4\)/d' < ukmers_clean1.sam > ukmers_clean.sam
-
-# Remove surplus fields
-awk '{print $1 "\t" $3 "\t" $9}' ukmers_clean.sam | sort -k2 -n > ukmers_clean_table
-
-# Get list for realignment
-awk '{print $3}' ukmers_clean_table > unique_kmers_both_sample_2
-
-# Convert to fasta
-awk '{print "> seq " ++count ORS $0}' unique_kmers_both_sample_2 > unique_kmers_both_sample_2.fa
-
-# Align
-bwa mem -a DA-386_06-X.fa unique_kmers_both_sample_2.fa > kmers_aligned_2.sam
-
-# Remove header
-sed '/^@/d' < kmers_aligned_2.sam > ukmers_ali.sam
-
-# See unique values of first colum
-awk '{ a[$1]++ } END { for (b in a) { print b } }' ukmers_ali.sam
-
-# Remove lines with more than one hit (next line is 256 or 272, then remove rest of 256/272)
-awk 'NR > 1 && !(/\t272\t/ || /\t256\t/) { print p } { p = $0 } END { print}' ukmers_ali.sam > ukmers_ali_clean1.sam
-sed '/^\t\(272\|256\|4\)/d' < ukmers_ali_clean1.sam > ukmers_ali_clean.sam
-
-# Remove surplus fields
-awk '{print $1 "\t" $3 "\t" $9}' ukmers_ali_clean.sam | sort -k2 -n > ukmers_ali_clean_table
-
-
-
-Final:
-
 ./kinv.sh -1 ../assemblies/2RL/DA-416_04-2RL.fa -2 ../assemblies/2RL/DA-402_03-2RL.fa -o temp2
 
+```
+
+## 2- Generate bed files of inversions detected between two paths on a GFA file.
+```
+./inv_from_paths -g ~/rds/rds-durbin-group-8b3VcZwY7rY/projects/funestus/graphs/output_all2RL_s100000_p90_g30004000_k30/all2RL.fa.gz.a944863.f043790.61cf8a6.smooth.fix.gfa -a DA-402_03-2RL -b DA-416_04-2RL -c 2RL -p 0.8 -s 50
 ```
--- old/inv_from_paths_gen.R
+++ old/inv_from_paths_gen.R
@@ -1,30 +1,39 @@
 #!/usr/bin/env Rscript
 
 library(data.table)
-library(Biostrings)
+#library(Biostrings)
 library(stringi)
 library(intervals)
 
-# args = commandArgs(trailingOnly=TRUE)
+args = commandArgs(trailingOnly=TRUE)
+min_inv_per <- 0.8
+min_inv_size <- 50
 
-args <- c("~/rds/rds-durbin-group-8b3VcZwY7rY/projects/funestus/graphs/output_all2RL_s100000_p90_g30004000_k30/all2RL.fa.gz.a944863.f043790.61cf8a6.smooth.fix.gfa", "DA-402_03-2RL", "DA-416_04-2RL", "2RL")
+# args <- c("~/rds/rds-durbin-group-8b3VcZwY7rY/projects/funestus/graphs/output_all2RL_s100000_p90_g30004000_k30/all2RL.fa.gz.a944863.f043790.61cf8a6.smooth.fix.gfa", "DA-402_03-2RL", "DA-416_04-2RL", "2RL")
+
+print(paste("GFA file: ", args[1]))
+print(paste("Path 1: ", args[2]))
+print(paste("Path 2: ", args[3]))
+print(paste("Chr: ", args[4]))
+
+print("Extracting segments...")
 
 cmd=paste("cat ",args[1], "| grep '^S' > segments",sep='')
 system(cmd)
+
+print("Extracting paths...")
 
 cmd=paste("cat ",args[1], " | grep \"^P\" | grep ",args[2], " | tr -s [:space:][,] \\\\n  > path_1" ,sep='')
 system(cmd)
 cmd=paste("cat ",args[1], " | grep \"^P\" | grep ",args[3], " | tr -s [:space:][,] \\\\n  > path_2" ,sep='')
 system(cmd)
 
-
+print("Loading paths and segments...")
+# Loading the paths
 
 t1 <- fread("path_1", skip =1)
-
 t2 <- fread("path_2", skip =1)
-
-nrow(t1)
-nrow(t2)
+s <- fread("segments")
 
 # removes end node (*)
 
@@ -34,7 +43,7 @@ t2<- t2[-nrow(t2),]
 colnames(t1)[1] <- "seg"
 colnames(t2)[1] <- "seg"
 
-# Add path pos variable
+# Add variable with position of segment in the path
 t1$pos <- as.numeric(rownames(t1))
 t2$pos2 <- as.numeric(rownames(t2))
 
@@ -44,6 +53,8 @@ change_sign <- function(seg) {
           paste(stri_sub(seg,1,-2),"-",sep=""),
           paste(stri_sub(seg,1,-2),"+",sep=""))
 }
+
+print("Selecting inverted nodes...")
 
 # Get list of inverted nodes
 
@@ -55,6 +66,8 @@ t1_inv <- t1[t1$inv %in% t2$seg, ]
 # order by position on the path
 t1_inv <- t1_inv[order(pos)]
 
+print("Merging consecutive nodes...")
+
 # Merge those that are consecutive (or chose another treshold)
 t1_clus <- clusters(t1_inv$pos,1)
 
@@ -62,21 +75,26 @@ t1_clus <- clusters(t1_inv$pos,1)
 t1_coord <- data.table(start=sapply(t1_clus, head, 1), end=sapply(t1_clus, tail, 1))
 
 
-s <- fread("segments")
+# Getting length of segments
 
 s[,l:=nchar(s$V3)]
 
+# Generating segment var to get lengths on paths
 
 t1[,sega:=as.numeric(stri_sub(seg,1,-2))]
 
 t2[,sega:=as.numeric(stri_sub(seg,1,-2))]
 
-#  get length of segment on t1
+#  get length of segments on paths tables
 setkey(s,V2)
 setkey(t1,sega)
 setkey(t2,sega)
 t1[s,len:=l]
 t2[s,len2:=l]
+
+print("Obtaining coordinates for path 1...")
+
+# Get coordinates of segments on paths tables
 
 setkey(t1,pos)
 setkey(t2,pos2)
@@ -85,18 +103,18 @@ t1$start <- 1+t1$end-t1$len
 t2$end2 <- cumsum(t2$len2)
 t2$start2 <- 1+t2$end2-t2$len2
 
+# Add coordinates to inversions table
+
 setkey(t1_coord,start)
 t1_coord[t1,s:=start]
-
-
 setkey(t1_coord,end)
 t1_coord[t1,e:=end]
 t1_coord[,l:=1+e-s]
 
-
+print(paste("Merge inversions when inverted > ",min_inv_per*100,"% of total sequence...",sep=""))
 
 # Loop to merge inversions closer than x% of the size
-min_inv_per <- 0.8
+
 t1_merged <- data.table(start=integer(), end=integer())
 current <- c(t1_coord[1]$s,t1_coord[1]$e)
 current_l <- t1_coord[1]$l
@@ -117,12 +135,16 @@ for (i in 2:nrow(t1_coord)) {
 }
 t1_merged <- rbindlist(list(t1_merged, data.table(start=current[1],end=current[2])))
 
+# Get size of merged intervals (in path 1)
 t1_merged[,l:=1+end-start]
 
 # Filter by min inversion size
 
-min_inv_size <- 50
+
 t1_merged <- t1_merged[l>min_inv_size]
+
+print("Obtaining coordinates for path 1...")
+# Obtain coordinates on path 2. pos1 -> seg -> pos2 -> coord2
 
 setkey(t1_merged,start)
 setkey(t1_coord,s)
@@ -142,6 +164,8 @@ setkey(t1_merged,seg_e)
 t1_merged[t2,start2:= start2]
 
 t1_merged[,l2:=1+end2-start2]
+
+# Write bed files 
 
 b1 <- data.table(chr=args[4], start = t1_merged$start-1, end = t1_merged$end)
 b1[,name:=paste("inv_",rownames(b1),sep="")]
